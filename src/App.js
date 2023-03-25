979c35b05db1263e75db2a08daa0a9c6929d01c5
alemughal <alimug38@gmail.com>
2023-03-25 20:07:23 +0500

    Changing

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
       {movies.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie} key={movie.imdbID} />
+            <MovieCard movie={movie}/>
           ))}
         </div>
       ) : (
