6493b0828d9411d3490ecace5d2ccc2400949a02
alemughal <alimug38@gmail.com>
2023-03-25 19:19:21 +0500

    Creating MovieCatd Component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
-import React, { useEffect } from "react";
-// import searchIcon from "./search.svg";
-import "./App.css"; 
+import React, { useState, useEffect } from "react";
+import MovieCard from "./MovieCard";
+import searchIcon from "./search.svg";
+import "./App.css";
 
-
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=a70530df';
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=a70530df";
+const movie1 = {
+  Title: "Italian Spiderman",
+  Year: "2007",
+  imdbID: "tt2705436",
+  Type: "movie",
+  Poster: "N/A",
+};
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    if(data.Response === 'True') {
-      console.log(data.Search);
+    if (data.Response === "True") {
+      setMovies(data.Search);
     }
   };
-  
-    useEffect(() => {
-      searchMovies('Spiderman');
-    }, []);
-    
 
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
   return (
     <div className="app">
-      <h1>Hello World</h1>
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for a movie"
+          value="Superman"
+          onChange={() => {}}
+        />
+        <img src={searchIcon} alt="search-icon" onClick={() => {}} />
+      </div>
+      {movies.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} key={movie.imdbID} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 };
