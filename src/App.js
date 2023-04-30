0d2b06df2cdb732fa1044d7392406c107abd18ca
alemughal <alimug38@gmail.com>
2023-04-30 13:51:20 +0500

    Some Changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import MovieCard from "./MovieCard";
 import searchIcon from "./search.svg";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=a70530df";
+const API_URL = "https://www.omdbapi.com/?i=tt3896198&apikey=a70530df";
 
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [search, setSearch] = useState("");
+
+  console.log(movies);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -40,18 +42,16 @@ const App = () => {
       {movies.length > 0 ? (
         <div className="container">
           {movies.map((movie) => (
-            <MovieCard movie={movie}/>
+            <MovieCard key={movie.imdbID} movie={movie}/>
           ))}
         </div>
       ) : (
-        <div className="container">
-          {movies.map((movie) => (
-            <MovieCard movie={movie}/>
-          ))}
-        </div>
+        <p>No results found.</p>
       )}
     </div>
   );
 };
+
+
 
 export default App;
